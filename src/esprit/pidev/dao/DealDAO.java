585919510b5dc13867424d516de7af117355e08a
Mahdi <Mahdi@PC-de-Mahdi>
2014-02-17 14:24:57 +0100

    ajout variable mahdi

---
+++ src/esprit/pidev/dao/DealDAO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esprit.pidev.dao;
+
+/**
+ *
+ * @author Wajdi
+ */
+public class DealDAO {
+String a;
+String b;
+String c;
+String dfgde;
+String sdgezr;
+String czqergterzg;
+String c2;
+String mahdi;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
