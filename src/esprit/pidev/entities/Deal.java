ff8c05b053fb888266c4b06e6f19b959631ac029
Mahdi <Mahdi@PC-de-Mahdi>
2014-02-17 15:04:54 +0100

    ajout variable mahdi

---
+++ src/esprit/pidev/entities/Deal.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esprit.pidev.entities;
+
+/**
+ *
+ * @author Mahdi
+ */
+public class Deal {
+    String b111111111;
+    
+}
---
+++ src/esprit/pidev/tests/mahdi.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esprit.pidev.tests;
+
+/**
+ *
+ * @author Mahdi
+ */
+public class mahdi {
+    
+}
